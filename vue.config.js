ce44baeb2e51219afaff902395c12e22a2c36151
OlehKobrynovych <o.kobrynovych@gmail.com>
2022-05-29 21:58:39 +0300

    updated

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: '/Vue_JsonPlaceholder_deploy_gh-pages/'
-}
+    publicPath: process.env.NODE_ENV === "production" ? "/Vue_JsonPlaceholder_deploy_gh-pages/" : "/",
+};
